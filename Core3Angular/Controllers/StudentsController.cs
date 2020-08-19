c3221e995e5de0ba039a135f8a4dc1e10088bbc0
PRT585 <pitroda.darshana@outlook.com>
2020-08-19 11:58:38 +0930

    Web api in Angular 8

---
+++ Core3Angular/Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core3Angular.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Core3Angular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            List<Student> oStudents = new List<Student>()
+            { 
+                new Student(){ Id = 1, Name = "Jimmy", Roll = 23412 },
+                new Student(){ Id = 2, Name = "Princy", Roll = 32321},
+                new Student(){ Id = 3, Name = "Lisa", Roll = 2232},
+            };
+            return oStudents;
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
