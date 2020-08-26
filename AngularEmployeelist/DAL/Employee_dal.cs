dc3009066be58e87407b57c4b5d20087a819f4c9
PRT585 <pitroda.darshana@outlook.com>
2020-08-26 15:57:14 +0930

    project added

---
+++ AngularEmployeelist/DAL/Employee_dal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularEmployeelist.DAL
+{
+    public class Employee_dal
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public bool IsRetired { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime LastModifiedOn { get; set; }
+    }
+}
+
